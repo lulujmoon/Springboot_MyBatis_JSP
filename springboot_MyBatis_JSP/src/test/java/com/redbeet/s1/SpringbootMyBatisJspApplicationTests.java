09732519dd8c843b1b6c74bcd6e47046a7746f9b
lulujmoon <80494527+lulujmoon@users.noreply.github.com>
2021-05-10 16:40:22 +0900

    init~

---
+++ springboot_MyBatis_JSP/src/test/java/com/redbeet/s1/SpringbootMyBatisJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redbeet.s1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMyBatisJspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
